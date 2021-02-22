40dd77c2dbbe386f1594f2c0acece07ff7483844
Mehdi Bounya <mehdi.bounya@gmail.com>
2021-02-22 18:23:51 +0100

    Add validation to Configuration

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -2,9 +2,12 @@
 
 namespace Mehdibo\Bundle\PasetoBundle\DependencyInjection;
 
+use ParagonIE\ConstantTime\Binary;
+use ParagonIE\Paseto\Protocol\Version2;
 use Symfony\Component\Config\Definition\Builder\NodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
 
 final class Configuration implements ConfigurationInterface
 {
@@ -19,6 +22,19 @@ final class Configuration implements ConfigurationInterface
         return $treeBuilder;
     }
 
+    private function validateKey(string $paramKey, string $value): string {
+        if (!\ctype_xdigit($value)) {
+            throw new InvalidConfigurationException(
+                "'{$paramKey}' must be a hex encoded key"
+            );
+        }
+        $decoded = \hex2bin($value);
+        if ($decoded === false) {
+            throw new InvalidConfigurationException("Failed converting {$paramKey} to binary");
+        }
+        return $decoded;
+    }
+
     private function createKeysNode(): NodeDefinition
     {
         $treeBuilder = new TreeBuilder('secret_keys');
@@ -28,12 +44,48 @@ final class Configuration implements ConfigurationInterface
             ->children()
                 ->scalarNode('symmetric_key')
                     ->info('A HEX encoded key used for local Paseto tokens')
+                    ->validate()
+                        ->always(function ($value) {
+                            $paramKey = 'mehdibo_paseto.secret_keys.symmetric_key';
+                            $decoded = $this->validateKey($paramKey, $value);
+                            $keyLen = Binary::safeStrlen($decoded);
+                            if ($keyLen !== Version2::SYMMETRIC_KEY_BYTES) {
+                                throw new InvalidConfigurationException(
+                                    sprintf(
+                                        "'%s' must be %d bytes long, %d found",
+                                        $paramKey,
+                                        Version2::SYMMETRIC_KEY_BYTES,
+                                        $keyLen
+                                    )
+                                );
+                            }
+                            return $decoded;
+                        })
+                    ->end()
                 ->end()
                 ->scalarNode('asymmetric_key')
                     ->info('A HEX encoded key used for public Paseto tokens')
+                    ->validate()
+                    ->always(function ($value) {
+                        $paramKey = 'mehdibo_paseto.secret_keys.asymmetric_key';
+                        $decoded = $this->validateKey($paramKey, $value);
+                        $keyLen = Binary::safeStrlen($decoded);
+                        if ($keyLen !== \SODIUM_CRYPTO_SIGN_SECRETKEYBYTES && $keyLen !== SODIUM_CRYPTO_SIGN_SEEDBYTES) {
+                            throw new InvalidConfigurationException(
+                                sprintf(
+                                    "'%s' must be %d or %d bytes long, %d found",
+                                    $paramKey,
+                                    \SODIUM_CRYPTO_SIGN_SECRETKEYBYTES,
+                                    \SODIUM_CRYPTO_SIGN_SEEDBYTES,
+                                    $keyLen
+                                )
+                            );
+                        }
+                        return $decoded;
+                    })
+                    ->end()
                 ->end()
             ->end();
-
         return $node;
     }
 }
