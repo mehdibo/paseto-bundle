88886db5cef281ead70b590baed63446970d3d03
Mehdi Bounya <mehdi.bounya@gmail.com>
2021-02-22 19:35:34 +0100

    Create KeysFactory

---
+++ src/Factories/KeysFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace Mehdibo\Bundle\PasetoBundle\Factories;
+
+
+use ParagonIE\Paseto\Keys\AsymmetricPublicKey;
+use ParagonIE\Paseto\Keys\AsymmetricSecretKey;
+use ParagonIE\Paseto\Keys\SymmetricKey;
+
+class KeysFactory
+{
+
+    public static function symmetricKeyFactory(string $key): SymmetricKey
+    {
+        return new SymmetricKey($key);
+    }
+
+    public static function asymmetricSecretKeyFactory(string $key): AsymmetricSecretKey
+    {
+        return new AsymmetricSecretKey($key);
+    }
+
+    public static function asymmetricPublicKeyFactory(string $key): AsymmetricPublicKey
+    {
+        $asymmetricSecretKey = new AsymmetricSecretKey($key);
+        return $asymmetricSecretKey->getPublicKey();
+    }
+
+}
