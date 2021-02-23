b73a480922ef8790c7143b823a3f356350df4f7d
Mehdi Bounya <mehdi.bounya@gmail.com>
2021-02-23 10:35:32 +0100

    Update GenerateAsymmetricKeyTest

--- tests/Command/GenerateAsymmetricKeyTest.php
+++ tests/Command/GenerateAsymmetricKeyTest.php
@@ -5,6 +5,9 @@ namespace Mehdibo\Bundle\PasetoBundle\Tests\Command;
 
 
 use Mehdibo\Bundle\PasetoBundle\Command\GenerateAsymmetricKey;
+use Mehdibo\Bundle\PasetoBundle\Factories\PasetoBuilderFactory;
+use ParagonIE\Paseto\Keys\AsymmetricSecretKey;
+use ParagonIE\Paseto\Parser;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Tester\CommandTester;
@@ -12,20 +15,44 @@ use Symfony\Component\Console\Tester\CommandTester;
 class GenerateAsymmetricKeyTest extends TestCase
 {
 
-    public function testCommandShowsKeys(): void
+    private CommandTester $commandTester;
+
+    protected function setUp(): void
     {
         $command = new GenerateAsymmetricKey();
-        $commandTester = new CommandTester($command);
+        $this->commandTester = new CommandTester($command);
+    }
 
-        $this->assertEquals(Command::SUCCESS, $commandTester->execute([]));
+    public function testCommandShowsKeys(): void
+    {
 
-        $output = explode("\n", $commandTester->getDisplay());
+        $this->assertEquals(Command::SUCCESS, $this->commandTester->execute([]));
+
+        $output = explode("\n", $this->commandTester->getDisplay());
         $this->assertCount(5, $output);
         $this->assertEquals("Private key", $output[0]);
         $this->assertEquals(128, \strlen($output[1]));
         $this->assertEquals("Public key", $output[2]);
         $this->assertEquals(64, \strlen($output[3]));
         $this->assertEmpty($output[4]);
+    }
+
+    public function testCommandGeneratesValidKeys(): void
+    {
+        $this->commandTester->execute([]);
+        $output = explode("\n", $this->commandTester->getDisplay());
+
+        $secretKey = new AsymmetricSecretKey(\hex2bin($output[1]));
+        $publicKey = $secretKey->getPublicKey();
+
+        $builder = PasetoBuilderFactory::publicPasetoFactory($secretKey);
+        $builder->setClaims(['test' => 'value']);
+
+        // TODO: use parser factory
+        $parser = new Parser();
+        $parser->setKey($publicKey);
+
+        $this->assertEquals(['test' => 'value'], $parser->parse($builder->toString())->getClaims());
     }
 
 }
