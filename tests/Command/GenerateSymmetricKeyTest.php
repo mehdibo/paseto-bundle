9d6eea6eda4213f62f206d3b5dfc26e336a31f02
Mehdi Bounya <mehdi.bounya@gmail.com>
2021-02-23 10:33:12 +0100

    Update GenerateSymmetricKeyTest

--- tests/Command/GenerateSymmetricKeyTest.php
+++ tests/Command/GenerateSymmetricKeyTest.php
@@ -4,6 +4,9 @@ namespace Mehdibo\Bundle\PasetoBundle\Tests\Command;
 
 
 use Mehdibo\Bundle\PasetoBundle\Command\GenerateSymmetricKey;
+use Mehdibo\Bundle\PasetoBundle\Factories\PasetoBuilderFactory;
+use ParagonIE\Paseto\Keys\SymmetricKey;
+use ParagonIE\Paseto\Parser;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Tester\CommandTester;
@@ -11,17 +14,38 @@ use Symfony\Component\Console\Tester\CommandTester;
 class GenerateSymmetricKeyTest extends TestCase
 {
 
-    public function testCommandShowsKey(): void
+    private CommandTester $commandTester;
+
+    protected function setUp(): void
     {
         $command = new GenerateSymmetricKey();
-        $commandTester = new CommandTester($command);
+        $this->commandTester = new CommandTester($command);
+    }
 
-        $this->assertEquals(Command::SUCCESS, $commandTester->execute([]));
-
-        $output = explode("\n", $commandTester->getDisplay());
+    public function testCommandShowsKey(): void
+    {
+        $this->assertEquals(Command::SUCCESS, $this->commandTester->execute([]));
+        $output = explode("\n", $this->commandTester->getDisplay());
         $this->assertCount(2, $output);
         $this->assertEquals(64, \strlen($output[0]));
         $this->assertEmpty($output[1]);
+    }
+
+    public function testCommandGeneratesValidKeys(): void
+    {
+        $this->commandTester->execute([]);
+        $output = explode("\n", $this->commandTester->getDisplay());
+
+        $symmetricKey = new SymmetricKey(\hex2bin($output[0]));
+
+        $builder = PasetoBuilderFactory::localPasetoFactory($symmetricKey);
+        $builder->setClaims(['test' => 'value']);
+
+        // TODO: use parser factory
+        $parser = new Parser();
+        $parser->setKey($symmetricKey);
+
+        $this->assertEquals(['test' => 'value'], $parser->parse($builder->toString())->getClaims());
     }
 
 }
