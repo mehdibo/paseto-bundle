ad10d8b96940fa29ab13f72d35c4e4f8f54b2e0d
Mehdi Bounya <mehdi.bounya@gmail.com>
2021-02-23 16:50:03 +0100

    Create ParserFactory

---
+++ src/Factories/ParserFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace Mehdibo\Bundle\PasetoBundle\Factories;
+
+
+use Mehdibo\Bundle\PasetoBundle\Services\LocalPasetoParser;
+use Mehdibo\Bundle\PasetoBundle\Services\PublicPasetoParser;
+use ParagonIE\Paseto\Keys\AsymmetricPublicKey;
+use ParagonIE\Paseto\Keys\SymmetricKey;
+use ParagonIE\Paseto\Purpose;
+
+class ParserFactory
+{
+
+    public static function localParser(SymmetricKey $symmetricKey): LocalPasetoParser
+    {
+        $parser = new LocalPasetoParser();
+        $parser->setKey($symmetricKey)
+            ->setPurpose(Purpose::local());
+        return $parser;
+    }
+
+    public static function publicParser(AsymmetricPublicKey $publicKey): PublicPasetoParser
+    {
+        $parser = new PublicPasetoParser();
+        $parser->setKey($publicKey)
+            ->setPurpose(Purpose::public());
+        return $parser;
+    }
+}
