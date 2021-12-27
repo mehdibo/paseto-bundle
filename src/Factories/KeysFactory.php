f0ad1d44d5478e287b49dfc863a42b428665a1c1
Mehdi Bounya <mehdi.bounya@gmail.com>
2021-12-27 23:46:45 +0100

    Handle invalid keys in KeysFactory

--- src/Factories/KeysFactory.php
+++ src/Factories/KeysFactory.php
@@ -3,6 +3,7 @@
 
 namespace Mehdibo\Bundle\PasetoBundle\Factories;
 
+use Mehdibo\Bundle\PasetoBundle\Exception\InvalidKeyException;
 use ParagonIE\Paseto\Keys\AsymmetricPublicKey;
 use ParagonIE\Paseto\Keys\AsymmetricSecretKey;
 use ParagonIE\Paseto\Keys\SymmetricKey;
@@ -13,12 +14,18 @@ class KeysFactory
     public static function symmetricKeyFactory(string $hexKey): SymmetricKey
     {
         $key = \hex2bin($hexKey);
+        if ($key === false) {
+            throw new InvalidKeyException("Couldn't convert key from HEX to bin");
+        }
         return new SymmetricKey($key);
     }
 
     public static function asymmetricSecretKeyFactory(string $hexKey): AsymmetricSecretKey
     {
         $key = \hex2bin($hexKey);
+        if ($key === false) {
+            throw new InvalidKeyException("Couldn't convert key from HEX to bin");
+        }
         return new AsymmetricSecretKey($key);
     }
 
